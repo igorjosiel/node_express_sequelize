919468cf24bec73cfea0c4afa51405dcc93cd957
Igor Josiel <igorjosielvitalino@gmail.com>
2026-03-13 10:53:59 -0300

    feat: acrescenta o método para atualizar registros.

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -10,6 +10,23 @@ class Controller {
       return res.status(200).json(registersList);
     } catch (error) {}
   }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    console.log(updatedData, id, req.body);
+
+    try {
+      const updatedRegister = await this.entityService.updateRegister(updatedData, Number(id));
+
+      if (!updatedRegister) {
+        return res.status(404).json({ message: "Registero não foi atualizado" });
+      }
+
+      return res.status(200).json({ message: "Atualizado com sucesso" });
+    } catch (error) {}
+  }
 }
 
 module.exports = Controller;
--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,19 @@ class Services {
   async getAllRegisters() {
     return dataSource[this.modelName].findAll();
   }
+
+  async updateRegister(updatedData, id) {
+    const updatedRegistersList = await dataSource[this.modelName].update(
+      updatedData,
+      { where: { id: id } },
+    );
+
+    if (updatedRegistersList[0] === 0) {
+      return false;
+    }
+
+    return true;
+  }
 }
 
 module.exports = Services;
