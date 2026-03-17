2209a8c09526136609e1b4133660c2f76f67c548
Igor Josiel <igorjosielvitalino@gmail.com>
2026-03-17 16:24:48 -0300

    feat: cria os outros métodos de crud no controlador para manipulação dos dados do banco.

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,7 +8,34 @@ class Controller {
       const registersList = await this.entityService.getAllRegisters();
 
       return res.status(200).json(registersList);
-    } catch (error) {}
+    } catch (error) {
+      // error
+    }
+  }
+
+  async getById(req, res) {
+    const { id } = req.params;
+
+    try {
+      const register = await this.entityService.getRegisterById(Number(id));
+
+      res.status(200).json(register);
+    } catch (error) {
+      // error
+    }
+  }
+
+  async create(req, res) {
+    const newRegisterData = req.body;
+
+    try {
+      const newRegister =
+        await this.entityService.createRegister(newRegisterData);
+
+      return res.status(201).json(newRegister);
+    } catch (error) {
+      // error
+    }
   }
 
   async update(req, res) {
@@ -18,14 +45,33 @@ class Controller {
     console.log(updatedData, id, req.body);
 
     try {
-      const updatedRegister = await this.entityService.updateRegister(updatedData, Number(id));
+      const updatedRegister = await this.entityService.updateRegister(
+        updatedData,
+        Number(id),
+      );
 
       if (!updatedRegister) {
-        return res.status(404).json({ message: "Registero não foi atualizado" });
+        return res
+          .status(404)
+          .json({ message: "Registero não foi atualizado" });
       }
 
       return res.status(200).json({ message: "Atualizado com sucesso" });
-    } catch (error) {}
+    } catch (error) {
+      // error
+    }
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    try {
+      await this.entityService.deleteRegister(Number(id));
+
+      return res.status(200).json({ message: "Deletado com sucesso" });
+    } catch (error) {
+      // error
+    }
   }
 }
 
